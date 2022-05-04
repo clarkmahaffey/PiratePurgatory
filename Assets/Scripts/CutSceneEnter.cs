285525603fd7e7a6a443743a3ecf127573234c63
Clark Mahaffey <clarkmahaffey@gmail.com>
2022-05-03 23:18:24 -0400

    Fade to Black, Walkway, Collisions

--- Assets/Scripts/CutSceneEnter.cs
+++ Assets/Scripts/CutSceneEnter.cs
@@ -9,15 +9,30 @@ public class CutSceneEnter : MonoBehaviour
     public GameObject little_ghost;
     public AudioSource source;
     public AudioClip clip;
+    public CanvasGroup fade;
+    public bool inCutscene;
     [SerializeField] private Animator Ghost;
     [SerializeField] private string GhostMove = "Ascend";
+    
+    void Start() {
+     fade.alpha = 0;
+     inCutscene = false;
+    }
 
     void OnTriggerEnter(Collider other)
     {
+        inCutscene = true;
         cutsceneCam.SetActive(true);
         thePlayer.SetActive(false);
         little_ghost.SetActive(true);
         Ghost.Play(GhostMove, 0, 0.0f);
         source.Play();
+        fade.alpha = 0.005f;
+    }
+    
+    void Update() {
+        if (inCutscene) {
+            fade.alpha += 0.002f;
+        }
     }
 }
